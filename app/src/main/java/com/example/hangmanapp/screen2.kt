c200203a59412cc4e0aee6d82cf02a606603cb80
Alberto Rodellar <=>
2024-11-29 17:42:44 +0100

    feat: added help button to menu screen

--- app/src/main/java/com/example/hangmanapp/screen2.kt
+++ app/src/main/java/com/example/hangmanapp/screen2.kt
@@ -1,27 +1,222 @@
 package com.example.hangmanapp
 
+import android.widget.Space
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun Screen2(navController: NavController) {
+    var selectedText by remember { mutableStateOf("") }
+
     Box(
-        modifier = Modifier.fillMaxSize().background(Color.Yellow)
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
     ) {
-        Text(
-            text = "Pantalla 2",
-            modifier = Modifier
-                .align(Alignment.Center)
-                .clickable
-                { navController.navigate(Routes.Pantalla3.route) })
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "HANGMAN\nGAME",
+                fontSize = 48.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+            Image(
+                painter = painterResource(id = R.drawable.hangman_game),
+                contentDescription = "Hangman Game Logo",
+                modifier = Modifier.size(200.dp)
+            )
+            Spacer(modifier = Modifier.height(48.dp))
+            DifficultyDropdown(selectedText = selectedText, onSelectionChange = { selectedText = it })
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Button(
+                    onClick = { navController.navigate(Routes.Pantalla3.route) },
+                    enabled = selectedText.isNotEmpty(),
+                    modifier = Modifier.width(150.dp)
+                        .padding(top = 32.dp)
+                ) {
+                    Text(
+                        text = "Play"
+                    )
+                }
+                HelpButtonWithModal()
+            }
+
+        }
     }
+}
+
+@Composable
+fun DifficultyDropdown(selectedText: String, onSelectionChange: (String) -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+    val difficulty = listOf("Fácil", "Medio", "Difícil")
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 48.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = Color(0xFFF1F1F1),
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .border(1.dp, Color.Gray, RoundedCornerShape(12.dp))
+                .clickable { expanded = !expanded }
+                .padding(vertical = 12.dp, horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = selectedText.ifEmpty { "Selecciona la dificultad" },
+                color = Color.Black,
+                modifier = Modifier.weight(1f)
+            )
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowDown,
+                contentDescription = "Arrow down",
+                modifier = Modifier.size(24.dp)
+            )
+        }
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            difficulty.forEach { diff ->
+                DropdownMenuItem(
+                    text = { Text(text = diff) },
+                    onClick = {
+                        expanded = false
+                        onSelectionChange(diff)
+                    }
+                )
+
+            }
+        }
+    }
+}
+
+@Composable
+fun HelpButtonWithModal() {
+    var showDialog by remember { mutableStateOf(false) }
+
+    Column(
+    ) {
+        Button(
+            onClick = { showDialog = true },
+            modifier = Modifier
+                .width(150.dp)
+                .padding(top = 8.dp)
+        ) {
+            Text(text = "Help")
+        }
+
+        if (showDialog) {
+            Dialog(onDismissRequest = { showDialog = false }) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                    shape = RoundedCornerShape(16.dp),
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+                    ) {
+                        Row(
+                        ) {
+                            Spacer(modifier = Modifier.weight(1f))
+                            IconButton(
+                                onClick = { showDialog = false }
+                            ) {
+                                Icon(
+                                    Icons.Filled.Close,
+                                    contentDescription = "Cerrar"
+                                )
+                            }
+                        }
+                        Column (
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ){
+                            Text(
+                                text = "Cómo jugar",
+                                fontSize = 36.sp,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.padding(bottom = 16.dp)
+                            )
+                        }
+                        Text(
+                            text = "Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet\n"+
+                            "Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet",
+                                    style = MaterialTheme.typography.bodyMedium,
+                            modifier = Modifier.padding(bottom = 16.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun Screen2Preview() {
+    val mockNavController = rememberNavController()
+    Screen2(navController = mockNavController)
 }
