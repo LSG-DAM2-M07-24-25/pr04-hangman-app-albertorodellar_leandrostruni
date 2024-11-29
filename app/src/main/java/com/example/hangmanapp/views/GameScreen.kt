a86f646209c4a5e15e6c8b187eea2dd2ac2d5e3d
Leandro Struni <164394186+LeanEmanuel@users.noreply.github.com>
2024-11-29 17:43:43 +0100

    feat: Add Image and test Button

--- app/src/main/java/com/example/hangmanapp/views/GameScreen.kt
+++ app/src/main/java/com/example/hangmanapp/views/GameScreen.kt
@@ -1,31 +1,71 @@
 package com.example.hangmanapp.views
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.hangmanapp.R
 import com.example.hangmanapp.model.Routes
 
 
-
-/*
 @Composable
 fun Screen3(navController: NavController) {
     Box(
-        modifier = Modifier.fillMaxSize().background(Color.Red)
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = "Pantalla 3",
-            modifier = Modifier
-                .align(Alignment.Center)
-                .clickable
-                { navController.navigate(Routes.Pantalla1.route) })
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+            Text(
+                text = "Game Screen",
+                modifier = Modifier
+                    .clickable
+                    { navController.navigate(Routes.Pantalla1.route) })
+            Image(
+                painter = painterResource(id = R.drawable.hangman_game),
+                contentDescription = "Hangman Game Logo",
+                modifier = Modifier.size(250.dp)
+            )
+
+
+            LetterButton()
+
+
+        }
+
     }
 }
-*/
+
+@Composable
+fun LetterButton() {
+    Button(
+        onClick = { /*TODO*/ }
+    ) {
+        Text(text = "A")
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun Screen3Preview() {
+    val mockNavController = rememberNavController()
+    Screen3(navController = mockNavController)
+}
